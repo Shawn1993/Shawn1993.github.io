82754acca2556bc069ce50082f2fb164c3492912
wuxiaohui <343270711@qq.com>
2016-12-01 13:40:24 +0800

    test

--- styles/js/lessismore.js
+++ styles/js/lessismore.js
@@ -3,29 +3,34 @@
  */
 
 
-! function($) {
+!function ($) {
 
-    $(function() {
+  $(function () {
+      
+		orderTheLeftNavigations();
 
-        orderTheLeftNavigations();
+      	function orderTheLeftNavigations(){
+  		    $('#navigation .sidenav').html($("#markdown-toc").html());
+  		    $('#navigation .sidenav ul').addClass("nav");
+  		    $("#markdown-toc").remove();
 
-        function orderTheLeftNavigations() {
-            $('#navigation .sidenav').html($("#markdown-toc").html());
-            $('#navigation .sidenav ul').addClass("nav");
-            $("#markdown-toc").remove();
+          // 添加Bootstrap表格样式 table-hover 
+          $(".docs-content table").addClass("table table-hover");
+      	}
 
-            // 添加Bootstrap表格样式 table-hover 
-            $(".docs-content table").addClass("table table-hover");
-        }
+      	$(window).load(repairTheImagesWhichCrossTheMaxWidth);
 
-        $(window).load(repairTheImagesWhichCrossTheMaxWidth);
-
-        function repairTheImagesWhichCrossTheMaxWidth() {
-            var images = $(".docs-content article img");
-            if (images != undefined) {
-                images.width("100%");
-            }
-        }
-    })
+      	function repairTheImagesWhichCrossTheMaxWidth(){
+      		var images = $(".docs-content img");
+      		if(images != undefined && images.length > 0){
+      			for(var i=0; i< images.length;i++){
+      				var imgWidth = images[i].width;
+      				if( imgWidth >= 757 ){
+      					 images[i].width = 757;
+      				}
+      			}
+      		}
+      	}
+  })
 
 }(jQuery)
